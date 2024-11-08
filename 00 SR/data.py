e6010d9166521db149418d0f39a479b497a578a0
Sanekim <sane2710@gmail.com>
2024-11-08 15:28:10 +0900

    xView data.py

--- 00 SR/data.py
+++ 00 SR/data.py
@@ -145,6 +145,130 @@ class DIV2K:
         for _ in ds: pass
         print(f'Cached decoded images in {cache_file}.')
 
+class xView:
+    def __init__(self,
+                 scale=4,
+                 subset='train',
+                 downgrade='bicubic',
+                 images_dir='.xView/images',
+                 caches_dir='.xView/caches'):
+        # scale
+        _scales = [2, 3, 4, 8]
+        if scale in _scales:
+            self.scale = scale
+        else:
+            raise ValueError(f'scale must be in ${_scales}')
+
+        self.subset = subset
+        # fix: 파일 인덱싱 필요
+        if subset == 'train':
+            self.image_ids = range(1, 801)
+        elif subset == 'valid':
+            self.image_ids = range(801, 901)
+        else:
+            raise ValueError("subset must be 'train' or 'valid'")
+        
+        self.images_dir = images_dir
+        os.makedirs(images_dir, exist_ok=True)
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def dataset(self, batch_size=16, repeat_count=None, random_transform=True):
+        ds = tf.data.Dataset.zip((self.lr_dataset(), self.hr_dataset()))
+        if random_transform:
+            ds = ds.map(lambda lr, hr: random_crop(lr, hr, scale=self.scale), num_parallel_calls=AUTOTUNE)
+            ds = ds.map(random_rotate, num_parallel_calls=AUTOTUNE)
+            ds = ds.map(random_flip, num_parallel_calls=AUTOTUNE)
+        ds = ds.batch(batch_size)
+        ds = ds.repeat(repeat_count)
+        ds = ds.prefetch(buffer_size=AUTOTUNE)
+        return ds
+    
+    def hr_dataset(self):
+        if not os.path.exists(self._hr_images_dir()):
+            # fix: image 다운로드 필요
+            # download_archive(self._hr_images_archive(), self.images_dir, extract=True)
+            pass
+
+        ds = self._images_dataset(self._hr_image_files()).cache(self._hr_cache_file())
+
+        if not os.path.exists(self._hr_cache_index()):
+            self._populate_cache(ds, self._hr_cache_file())
+
+        return ds
+    
+    def _hr_images_dir(self):
+        return os.path.join(self.images_dir, f'xView_{self.subset}_HR')
+    
+    def _hr_image_files(self):
+        images_dir = self._hr_images_dir()
+        return [os.path.join(images_dir, f'{image_id:04}.png') for image_id in self.image_ids] # fix: 파일 이름 수정 필요
+
+    def lr_dataset(self):
+        if not os.path.exists(self._lr_images_dir()):
+            # fix: image HR에서 다운스케일링 직접
+            self._lr_image_generate()
+
+        ds = self._images_dataset(self._lr_image_files()).cache(self._lr_cache_file())
+
+        if not os.path.exists(self._lr_cache_index()):
+            self._populate_cache(ds, self._lr_cache_file())
+
+        return ds
+    
+    def _lr_images_dir(self):
+        return os.path.join(self.images_dir, f'xView_{self.subset}_LR_{self.downgrade}_X{self.scale}')
+    
+    def _lr_image_files(self):
+        images_dir = self._lr_images_dir()
+        return [os.path.join(images_dir, f'{image_id:04}.png') for image_id in self.image_ids] # fix: 파일 이름 수정 필요
+
+    def _lr_image_generate(self):
+        hr = self.hr_dataset()
+
+        def resize_image(image):
+            original_shape = tf.shape(image)[:2]
+            new_shape = tf.cast(tf.cast(original_shape, tf.float32) / self.scale, tf.int32)
+            resized_image = tf.image.resize(image, new_shape, method=tf.image.ResizeMethod.BICUBIC) # BICUBIC?
+            return resized_image
+
+        lr = hr.map(resize_image, num_parallel_calls=AUTOTUNE)
+
+        # file_names 생성
+        lr_file_names = self._lr_image_files()
+
+        # lr 저장
+        for image, file_name in zip(lr, lr_file_names):
+            # 이미지 인코딩 (PNG 형식)
+            encoded_image = tf.io.encode_png(tf.cast(image, tf.uint8))
+            # 이미지 저장
+            tf.io.write_file(file_name, encoded_image)
+
+    def _hr_cache_index(self):
+        return f'{self._hr_cache_file()}.index'
+
+    def _lr_cache_index(self):
+        return f'{self._lr_cache_file()}.index'
+
+    def _hr_cache_file(self):
+        return os.path.join(self.caches_dir, f'DIV2K_{self.subset}_HR.cache')
+
+    def _lr_cache_file(self):
+        return os.path.join(self.caches_dir, f'DIV2K_{self.subset}_LR_{self.downgrade}_X{self.scale}.cache')
+
+    @staticmethod
+    def _images_dataset(image_files):
+        ds = tf.data.Dataset.from_tensor_slices(image_files)
+        ds = ds.map(tf.io.read_file)
+        ds = ds.map(lambda x: tf.image.decode_png(x, channels=3), num_parallel_calls=AUTOTUNE)
+        return ds
+    
+    @staticmethod
+    def _populate_cache(ds, cache_file):
+        print(f'Caching decoded images in {cache_file} ...')
+        for _ in ds: pass
+        print(f'Cached decoded images in {cache_file}.')
 
 # -----------------------------------------------------------
 #  Transformations
